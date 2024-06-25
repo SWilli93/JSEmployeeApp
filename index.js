1857c005551c7da8045d399154763d7d52c49e24
Scott <70402467+SWilli93@users.noreply.github.com>
2024-06-25 15:37:01 -0400

    import fs from node-promises, loadData and writeData functions

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-// Import Sample Data
-import employees from './data.json' assert { type: 'json' }
+import fs from 'node:fs/promises'
+
+// GLOBAL VARIABLES -------------------------------------
+
+let employees = [];
+
+// Loading and writing data to the filesystem -------------------------------------
+const loadData = async () => {
+  console.log("Loading employees.....")
+  try {
+    const fileData = await fs.readFile('./data.json');
+    employees = JSON.parse(fileData);
+  } catch (err) {
+    console.log("Cannot load in Employees");
+    throw err;
+  }
+}
+
+const writeData = async () => {
+  console.log("Writing employees....");
+    try {
+      fs.writeFile('./data.json', JSON.stringify(employees, null, 2));
+    } catch (err) {
+      console.error("Cannot write employees data.");
+      throw err;
+    }
+}
+
 
 import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
